833ed270c4084b6a0e48d6a705ae3ec199ddb112
zfrandom <zfrandomstuff@yahoo.com>
2017-04-28 16:13:45 -0500

    Add files via upload

---
+++ web/js.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });
